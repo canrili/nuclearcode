384df4e17ac7236b01666a6e86016a60a581f408
rd.zhaoxin <rd.zhaoxin@foxmail.com>
2021-05-25 14:50:27 +0800

    :construction: 手动生成构造参数

--- src/main/java/com/dddinjava/portal/entity/Account.java
+++ src/main/java/com/dddinjava/portal/entity/Account.java
@@ -12,21 +12,31 @@ import java.io.Serializable;
 @Getter
 @Setter
 @ToString
-@NoArgsConstructor
-@RequiredArgsConstructor
 public class Account implements Serializable {
 
   private static final long serialVersionUID = -4605836191379150771L;
 
   private Long id;
 
-  @NonNull
   private String username;
 
-  @NonNull
   private String password;
 
-  @NonNull
   private String ownerFullName;
 
+  public Account() {
+  }
+
+  public Account(String username, String password, String ownerFullName) {
+    this.username = username;
+    this.password = password;
+    this.ownerFullName = ownerFullName;
+  }
+
+  public Account(Long id, String username, String password, String ownerFullName) {
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.ownerFullName = ownerFullName;
+  }
 }
