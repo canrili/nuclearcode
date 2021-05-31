86e59a4c4838ff61851cd46f2eeba3e389914c20
rd.zhaoxin <rd.zhaoxin@foxmail.com>
2021-05-31 10:48:52 +0800

    :construction: 删除演示的代码。代码依旧可以在其他分支看到

--- src/main/java/com/dddinjava/portal/PortalApplication.java
+++ src/main/java/com/dddinjava/portal/PortalApplication.java
@@ -3,6 +3,10 @@ package com.dddinjava.portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author zhaox
+ */
 @SpringBootApplication
 public class PortalApplication {
 
