0692f29d6437ceef371d15ed3df422029b363921
rd.zhaoxin <rd.zhaoxin@foxmail.com>
2021-06-03 09:54:59 +0800

    :construction: redis配置

--- src/test/java/com/dddinjava/portal/PortalApplicationTests.java
+++ src/test/java/com/dddinjava/portal/PortalApplicationTests.java
@@ -1,13 +1,7 @@
 package com.dddinjava.portal;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootTest
 class PortalApplicationTests {
