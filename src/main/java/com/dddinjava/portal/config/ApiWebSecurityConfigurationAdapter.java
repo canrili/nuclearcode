e8df8ddb68d22768071808207f9b8b609b40f6d7
rd.zhaoxin <rd.zhaoxin@foxmail.com>
2021-06-06 09:54:58 +0800

    :construction: Spring Security 配置

--- src/main/java/com/dddinjava/portal/config/ApiWebSecurityConfigurationAdapter.java
+++ src/main/java/com/dddinjava/portal/config/ApiWebSecurityConfigurationAdapter.java
@@ -3,13 +3,6 @@ package com.dddinjava.portal.config;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * HttpSecurity配置
